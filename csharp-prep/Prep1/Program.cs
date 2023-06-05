33fc1e2ec4338518247676f46f1721801cc65f14
Matt Schmitt <schmitty4437@gmail.com>
2023-06-05 14:50:47 -0600

    Finished prep1. James Bond program.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        // program asks for first and last name. Displays name in James Bond style.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
